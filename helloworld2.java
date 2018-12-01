da9bc708015445cb28ee426595905ac673429206
IvySanchez418 <44900723+IvySanchez418@users.noreply.github.com>
2018-12-01 16:21:45 +0800

    Add files via upload

---
+++ helloworld2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class helloworld2 {
+
+    
+    public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter Hello World");
+	String name = sc.nextLine();
+	
+
+	}
+}
